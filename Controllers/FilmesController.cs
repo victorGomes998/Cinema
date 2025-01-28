54eb5c7b48b6cf7f7408651c84e1214789ffb69a
Victor Gomes <victor.gda07@gmail.com>
2025-01-28 17:22:03 -0300

    Implementação de respostas HTTP e paginação na busca

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -1,18 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Cinema.Model;
 
-namespace Cinema.FilmesController
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class FilmesController : ControllerBase
-    {
-        private static List<Filme> filmes = new List<Filme>();
+namespace Cinema.FilmesController;
 
-        [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
-        {
-            filmes.Add(filme);
-        }
+[ApiController]
+[Route("[controller]")]
+public class FilmesController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int Id = 0;
+    private static int pagina = -1;
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = Id++;
+        filmes.Add(filme);
+
+        return CreatedAtAction(nameof(BuscaFilmePorId), new { Id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> BuscaFilmes()
+    {
+        pagina++;
+
+        return filmes.Skip(10 * pagina).Take(10);
+    }
+
+    [HttpGet("{Id}")]
+    public IActionResult BuscaFilmePorId(int Id)
+    {
+        var filme = filmes.FirstOrDefault(a => a.Id == Id);
+
+        if (filme == null) return NotFound();
+
+        return Ok(filme);
     }
 }
--- Model/Filme.cs
+++ Model/Filme.cs
@@ -1,17 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Cinema.Model
+namespace Cinema.Model;
+
+public class Filme
 {
-    public class Filme
-    {
-        [Required(ErrorMessage = "O título do filme é obrigatório")]
-        public string Titulo { get; set; }
+    public int Id { get;set; } = 0;
 
-        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
-        public string Genero { get; set; }
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
 
-        [Required(ErrorMessage = "a duração do filme é obrigatório")]
-        [Range(60, 240, ErrorMessage = "Tamanho de filme inválido, este deve ser compreendido entre 60 e 240 minutos")]
-        public int Duracao { get; set; }
-    }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "a duração do filme é obrigatório")]
+    [Range(60, 240, ErrorMessage = "Tamanho de filme inválido, este deve ser compreendido entre 60 e 240 minutos")]
+    public int Duracao { get; set; }
 }
